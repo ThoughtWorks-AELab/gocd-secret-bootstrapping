466dacfac99e5a2fb14dd1d7f1b016436cef6c18
Daniel Somerfield <dsomerfi@thoughtworks.com>
2017-07-28 15:44:38 -0700

    Fixed path to artifact

--- bootstrap_pipeline.py
+++ bootstrap_pipeline.py
@@ -13,7 +13,7 @@ build_stage = pipeline.ensure_stage("build")
 build_stage.ensure_job("init")
 build_job = build_stage.ensure_job("build")
 build_job.add_task(ExecTask(['./gradlew', 'assemble']))
-build_job.ensure_artifacts({Artifact.get_build_artifact("./build/libs/hello-secret-world.jar")})
+build_job.ensure_artifacts({Artifact.get_build_artifact("build/libs/hello-secret-world.jar")})
 
 build_image_stage = pipeline.ensure_stage("build_image")
 build_docker_image_job = build_image_stage.ensure_job("build_docker_image")
