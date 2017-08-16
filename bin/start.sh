146ef904acddcff7e85a95ff2759fcbacefa9401
Daniel Somerfield <dsomerfi@thoughtworks.com>
2017-08-16 10:54:09 -0700

    [#672] Removed hard-coded username from kube files.

--- bin/start.sh
+++ bin/start.sh
@@ -4,12 +4,19 @@ set -e
 . ./.env
 . ./.pythonenv/bin/activate
 
+mkdir -p /tmp/gocd-secret-bootstrapping
+# Substitute the username from env variable
+sed -- 's/${REGISTRY_USERNAME}/'"${REGISTRY_USERNAME}"'/g' ./kubernetes/go-cd-agent.yml > /tmp/gocd-secret-bootstrapping/go-cd-agent.yml
+sed -- 's/${REGISTRY_USERNAME}/'"${REGISTRY_USERNAME}"'/g' ./kubernetes/vault.yml > /tmp/gocd-secret-bootstrapping/vault.yml
+sed -- 's/${REGISTRY_USERNAME}/'"${REGISTRY_USERNAME}"'/g' ./kubernetes/vault-setup.yml > /tmp/gocd-secret-bootstrapping/vault-setup.yml
+
+
 kubectl create -f ./kubernetes/go-cd-server.yml
 
 ./bin/set-register-key.py
 
-kubectl create -f ./kubernetes/go-cd-agent.yml
-kubectl create -f ./kubernetes/vault.yml
-kubectl create -f ./kubernetes/vault-setup.yml
+kubectl create -f /tmp/gocd-secret-bootstrapping/go-cd-agent.yml
+kubectl create -f /tmp/gocd-secret-bootstrapping/vault.yml
+kubectl create -f /tmp/gocd-secret-bootstrapping/vault-setup.yml
 
 minikube service list
